0701680cd8443bbe5c14046869e565dafe360bf7
Oliwier Minota <oliwierminota@gmail.com>
2019-12-16 19:03:06 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -96,13 +96,13 @@ WIZARDFILE = BUILDFILE
 #        Auto Install Repo If Not Installed             #
 #########################################################
 # Enable Auto Install 'Yes' or 'No'
-AUTOINSTALL = 'No'
+AUTOINSTALL = 'Yes'
 # Addon ID for the repository
-REPOID = 'repository.openwizard'
+REPOID = 'repository.multiwu'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML = 'https://raw.githubusercontent.com/drinfernoo/repository.openwizard/master/zips/addons.xml'
+REPOADDONXML = 'https://raw.githubusercontent.com/MultiWu/repository.multiwu/master/zips/addons.xml'
 # Url to folder zip is located in
-REPOZIPURL = 'https://raw.githubusercontent.com/drinfernoo/repository.openwizard/master/zips/repository.openwizard/'
+REPOZIPURL = 'https://raw.githubusercontent.com/MultiWu/repository.multiwu/master/zips/repository.openwizard/'
 #########################################################
 
 #########################################################
